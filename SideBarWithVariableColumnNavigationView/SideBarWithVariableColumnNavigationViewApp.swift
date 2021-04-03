c5196b9d7af8f59a491c7b86a93c99d92e80aa33
Ataias Pereira Reis <ataiasreis@gmail.com>
2021-04-03 14:48:31 -0300

    Initial Commit

---
+++ SideBarWithVariableColumnNavigationView/SideBarWithVariableColumnNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideBarWithVariableColumnNavigationViewApp.swift
+//  SideBarWithVariableColumnNavigationView
+//
+//  Created by Ataias Pereira Reis on 03/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SideBarWithVariableColumnNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
